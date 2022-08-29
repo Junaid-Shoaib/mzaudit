8e2d9cc0927fe241b47d2ec229432e860cb48a03
Junaid <sjuniad447@gmail.com>
2022-08-29 16:28:58 +0500

    Validation Advisor

--- resources/js/Pages/Advisor_accounts/Create.vue
+++ resources/js/Pages/Advisor_accounts/Create.vue
@@ -5,7 +5,7 @@
         Create Advisor Accounts
       </h2>
     </template>
-    <div v-if="$page.props.flash.success" class="bg-yellow-400 text-white">
+    <div v-if="$page.props.flash.success" class="bg-red-600 text-center text-white">
       {{ $page.props.flash.success }}
     </div>
     <div class="max-w-7xl mx-auto pb-2">
